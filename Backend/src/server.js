d7d9be8faa63e74b023b99f5bc5911c4a9f76ca1
Waseem Bagwan <waseembagwan950@gmail.com>
2025-08-04 12:14:32 +0530

    ready for deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,6 +4,9 @@ import { connectDB } from '../config/db.js';
 import dotenv from 'dotenv'
 import rateLimiter from '../middleware/rateLimiter.js';
 import cors from 'cors'
+import path from 'path'
+
+
 
 dotenv.config()
 
@@ -13,16 +16,31 @@ const app = express();
 
 const PORT = process.env.PORT || 5001;
 
+const __dirname = path.resolve()
+
 //middleware 
-app.use(cors({
-    //by default it allow all request from different frontend site if u want to specific frontend should send request you can name it's url or domain in origin
-    origin : 'http://localhost:5173'
-}))
+if(process.env.NODE_ENV !== 'production'){
+    app.use(cors({
+        //by default it allow all request from different frontend site if u want to specific frontend should send request you can name it's url or domain in origin
+        origin : 'http://localhost:5173'
+    }))
+}
 app.use(express.json())
 app.use(rateLimiter)
 
 
 app.use('/api/notes',noteRoute);
+
+if(process.env.NODE_ENV === 'production'){
+
+    app.use(express.static(path.join(__dirname,"../Frontend/dist")))
+
+    app.get('*',(req,res) => {
+        res.sendFile(path.join(__dirname,"../Frontend","dist","index.html"))
+    })
+}
+
+
 
 connectDB().then(() => {
     app.listen(PORT, () => {
